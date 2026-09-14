0029cf9b14b6974f44d58af5e00e1d67d92a3382
TedEbear100 <xerisbearis@icloud.com>
2026-09-14 11:22:05 -0700

    speed and jumpheight

--- Ehrhardt_2D_sneak/Assets/Scripts/PlayerConrtoller.cs
+++ Ehrhardt_2D_sneak/Assets/Scripts/PlayerConrtoller.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerConrtoller : MonoBehaviour
 {
+
+    public float speed = 5.0f;
+    public float jumpheight = 10.0f;
+
+    PlayerInput input;
+    Ridgebody rb; 
+
+    public Vector2 moveInput = Vector2.zero;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        input = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+
+
     }
 
     // Update is called once per frame
