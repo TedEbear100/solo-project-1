c85b525bd6a2079e8c60f0495c2d1a2ae6d601cb
TedEbear100 <xerisbearis@icloud.com>
2026-09-14 13:33:15 -0700

    movement/jump/camera
    movement and jump incomplete

--- Ehrhardt_2D_sneak/Assets/Scripts/PlayerConrtoller.cs
+++ Ehrhardt_2D_sneak/Assets/Scripts/PlayerConrtoller.cs
@@ -6,9 +6,12 @@ public class PlayerConrtoller : MonoBehaviour
 
     public float speed = 5.0f;
     public float jumpheight = 10.0f;
+    public float jumpdetectdistance;
 
     PlayerInput input;
-    Rigidbody rb; 
+    Rigidbody2D rb;
+
+    Ray jumpRay2D;
 
     public Vector2 moveInput = Vector2.zero;
 
@@ -16,14 +19,34 @@ public class PlayerConrtoller : MonoBehaviour
     void Start()
     {
         input = GetComponent<PlayerInput>();
-        rb = GetComponent<Rigidbody>();
-
-
+        rb = GetComponent<Rigidbody2D>();
+        jumpRay2D = new Ray2D();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        jumpRay2D.origin = transform.position;
+        jumpRay2D.direction = -transform.up;
+
+        rb.linearVelocityX = moveInput.x * speed;
+  
+    
     }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveInput.x = context.ReadValue<Vector2>().x;
+    }
+
+    public void Jump()
+    {
+       
+        if (Physics2D.Raycast(jumpRay.orgin, jumpRay.direction, jumpDetectDistance))
+            rb.AddForceY(jumpheight, ForceMode2D.Impulse);
+    }
+
+
+
 }
+
